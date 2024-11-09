e6fab297abe411f647dc9ae70a85d222ad989e6c
Xiaole Su <suxiaole0223@gmail.com>
2024-11-09 15:43:21 -0500

    doesnt work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import OceanPlatformer from "./game";
+import PixelArtAnalyzer from "./imgToCoord"; 
 
 export default function Home() {
   return (
     <div>
       <OceanPlatformer />
+      <PixelArtAnalyzer/>
+
     </div>
   );
 }
